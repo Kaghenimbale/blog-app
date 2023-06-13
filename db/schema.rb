2a5034e6537665d1ece97376f3238fa14c3f056b
Kagheni <Kaghenimbale@gmail.com>
2023-06-13 17:10:55 +0200

    Add the post table to the schemas.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_145828) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_150903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_145828) do
     t.index ["author_id"], name: "index_comments_on_author_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id_id", null: false
+    t.string "title"
+    t.text "Text"
+    t.integer "CommentsCounter"
+    t.integer "likesCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id_id"], name: "index_posts_on_author_id_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
@@ -33,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_145828) do
   end
 
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id_id"
 end
