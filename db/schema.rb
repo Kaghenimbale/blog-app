73145f6792aa518d2a720f6d05b8632044c10025
Kagheni <Kaghenimbale@gmail.com>
2023-06-14 15:45:46 +0200

    Add rails users comand generator

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_151633) do
   end
 
   # This is the likes table
-  rails generate migration CreateLike like_id:references post_id:references
+  # rails generate migration CreateLike like_id:references post_id:references
 
   create_table "likes", force: :cascade do |t|
     t.bigint "like_id_id", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_151633) do
   end
 
   # This is the users table
+  # rails generate migration CreateUsers name:string photo:string bio:text post_counter:integer
 
   create_table "users", force: :cascade do |t|
     t.string "name"
