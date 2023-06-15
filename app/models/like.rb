06a17d15e736d1b7440c0a3fda2b1a85af03e27e
Kagheni <Kaghenimbale@gmail.com>
2023-06-15 21:18:07 +0200

    Add the likes methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save :update_posts_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
+
 end
