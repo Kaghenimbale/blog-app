58afd0f2e4868c11c6de28cb9e6723017e4dee8b
Kagheni <Kaghenimbale@gmail.com>
2023-06-16 10:05:49 +0200

    Add foreign key on the models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :author , class_name: 'User'
-  belongs_to :post
+  belongs_to :author , class_name: 'User', foreign_key: :author_id
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
   after_save :comments_posts_counter
 
   private
 
   def comments_posts_counter
-    post.increment!(:likes_counter)
+    post.update!(comments_counter: post.comments.count)
   end
 end
