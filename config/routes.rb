ca567cbadc551ba91ec387661cf49f65c652e228
Kagheni <Kaghenimbale@gmail.com>
2023-06-19 15:18:05 +0200

    Add resources to the routes.config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 end
