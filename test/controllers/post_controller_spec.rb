bdeb2a158f4ac044f8d31e93f702a44eb51e803a
Kagheni <Kaghenimbale@gmail.com>
2023-06-20 09:58:34 +0200

    Add the post_spec controller test

--- test/controllers/post_controller_spec.rb
+++ test/controllers/post_controller_spec.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    # Any setup code specific to PostsControllerTest
-  end
-
   test 'should get index' do
     get posts_index_url
     assert_response :success
