140cde4c69f2f57ca7377958738734a9dba79958
Kagheni <Kaghenimbale@gmail.com>
2023-06-21 19:50:11 +0200

    Add post to the project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @author = User.find(params[:user_id])
-    @posts = @author.posts.includes(:comments)
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
