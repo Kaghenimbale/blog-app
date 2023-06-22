cb611db69b33341317374f2f5f077a419fbef55a
Kagheni <Kaghenimbale@gmail.com>
2023-06-22 14:34:48 +0200

    Modify the show @user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 end
