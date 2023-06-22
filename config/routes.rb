73a8c553de40dc43bf436361b635e9fa797e24cf
Kagheni <Kaghenimbale@gmail.com>
2023-06-22 14:37:43 +0200

    Add new and create action to the route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get '/posts/show', to: 'posts#show'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 end
