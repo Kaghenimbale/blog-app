32bb0bfcef4787f6729463e3bb8551a4844af360
Kagheni <Kaghenimbale@gmail.com>
2023-06-26 12:44:05 +0200

    Fix capybara problem in the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:id])
+    @user = @post.user.includes(:posts) 
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.includes(:posts)
+    @users = User.all
   end
 
   def show
