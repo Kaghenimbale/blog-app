a30d937b869c6eb572ec7f1cd54c118c45a1b2d5
danutibe07 <danielutibe07@gmail.com>
2023-06-26 15:41:16 +0100

    Added tests for post_show_spec.rb

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe 'Post show page', type: :system do
   let!(:users) do
     User.create([{ name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.' },
                  { name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from England.' },
-                 { name: 'Dan', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Analyst from .' },
+                 { name: 'Dan', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Analyst from Texas.' },
                  { name: 'Chris', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Web Developer.' }])
   end
   let!(:posts) do
     Post.create!([{ author: users[0], title: 'First Post', text: 'My first post' },
                   { author: users[1], title: 'Second Post', text: 'My Second post' },
-                  { author: users[2], title: 'Third Post', text: 'My Third post' }])
+                  { author: users[2], title: 'Third Post', text: 'My Third post' },
+                  { author: users[3], title: 'Fourth Post', text: 'My Fourth post' }])
   end
 
   describe 'Post show' do
@@ -30,6 +31,7 @@ RSpec.describe 'Post show page', type: :system do
       Like.create!(author: users[0], post: posts[0])
       Like.create!(author: users[1], post: posts[0])
       Like.create!(author: users[2], post: posts[0])
+      Like.create!(author: users[3], post: posts[0])
       visit user_post_path(users[0], posts[0])
       expect(page).to have_content(posts[0].likes_counter)
     end
