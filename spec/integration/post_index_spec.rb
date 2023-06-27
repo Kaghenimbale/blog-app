0b684e518130e9be4c2c56b6e736ff6a16e33b62
Kagheni <Kaghenimbale@gmail.com>
2023-06-27 09:40:21 +0200

    Add testing for the post index file

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -1,29 +1,68 @@
 require 'rails_helper'
 
-RSpec.describe 'User post index page', type: :system do
-  let!(:user) do
-    User.create(name: 'John', photo: 'https://example.com/profile.jpg')
-    User.create(name: 'Daniel', photo: 'https://example.com/profile.jpg')
+RSpec.describe 'Post index page', type: :system do
+  let!(:users) do
+    User.create([{ name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.' },
+                 { name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from England.' },
+                 { name: 'Chris', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Front end developer.' }])
   end
-
   let!(:posts) do
-    user.posts.create([
-                        { text: 'First post', comments_counter: 2, likes_counter: 5 },
-                        { text: 'Second post', comments_counter: 3, likes_counter: 7 },
-                        { text: 'Third post', comments_counter: 8, likes_counter: 17 }
-                      ])
+    Post.create!([{ author: users[0], title: 'First Post', text: 'My first post' },
+                  { author: users[1], title: 'Second Post', text: 'My Second post' },
+                  { author: users[2], title: 'Third Post', text: 'My Third post' }])
   end
 
-  it 'displays user information and posts' do
-    visit user_posts_path(user)
+  describe 'Post index' do
+    it 'shows user profile picture' do
+      visit user_posts_path(users[0])
+      expect(page).to have_css('img[alt="user_profile"]')
+    end
+    it 'shows user name' do
+      visit user_posts_path(users[0])
+      expect(page).to have_content(users[0].name)
+    end
 
-    expect(page).to have_button('Pagination')
-  end
+    it 'shows posts count' do
+      visit user_posts_path(users[0])
+      expect(page).to have_content(users[0].posts_counter)
+    end
 
-  it 'redirects to post show page when a post is clicked' do
-    visit user_posts_path(user)
-    expect(page).to have_css('.post-show')
-    expect(page).to have_content(user.name)
-    expect(page).to have_content("Number of post: #{user.posts_counter}")
+    it 'shows a post title' do
+      visit user_posts_path(users[0])
+      expect(page).to have_content(posts[0].text)
+    end
+
+    it 'shows some of a post body' do
+      visit user_posts_path(users[0])
+      expect(page).to have_content(posts[0].text)
+    end
+
+    it 'shows the first comment on a post' do
+      Comment.create!(author: users[0], post: posts[0], text: 'niceeee')
+      visit user_posts_path(users[0])
+      expect(page).to have_content(posts[0].comments[0].text)
+    end
+
+    it 'shows how many comments a post has' do
+      Comment.create!(author: users[0], post: posts[0], text: 'niceeee')
+      Comment.create!(author: users[1], post: posts[0], text: 'amazing')
+      visit user_posts_path(users[0])
+      expect(page).to have_content(posts[0].comments_counter)
+    end
+    it 'shows how many likes a post has' do
+      Like.create!(author: users[0], post: posts[0])
+      visit user_posts_path(users[0])
+      expect(page).to have_content(posts[0].likes_counter)
+    end
+    it 'shows pagination section' do
+      visit user_posts_path(users[0])
+      expect(page).to have_content('Pagination')
+    end
+    it 'When click on a post, it redirects to that post show page' do
+      visit user_posts_path(users[0])
+      click_on 'My first post'
+      expect(page).to have_content('My first post')
+    end
   end
 end
+
