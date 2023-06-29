965cb506c5131b15529cc661c1ac961706a1378a
danutibe07 <danielutibe07@gmail.com>
2023-06-29 21:05:48 +0100

    Updated Models post.rb and user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def admin?
+    role == 'admin'
+  end
+
   def recent_posts
     posts.order('created_at DESC').limit(3)
   end
