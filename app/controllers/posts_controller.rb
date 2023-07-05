a8017b93a0e3af73e9fff88f4c88e6418318d0b9
Kagheni <Kaghenimbale@gmail.com>
2023-07-05 10:56:42 +0200

    Add the modification about authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
   end
+
+  def create
+    post = current_user.posts.new(post_params)
+
+    if post.save
+      redirect_to user_posts_path, notice: 'Post created successfully!!'
+    else
+      flash.now[:alert] = 'Unable to create a post'
+      render :new
+    end
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.comments.destroy_all if @post.comments.exists?
+    @post.likes.destroy_all if @post.likes.exists?
+    @post.destroy
+    redirect_to user_posts_path(user_id: current_user.id)
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
 end
