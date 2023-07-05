e75cd4014c746ebbee8a0fcaab18821041ccf5c7
Kagheni <Kaghenimbale@gmail.com>
2023-07-05 11:06:42 +0200

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,12 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
   end
+
   def show
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
   end
+
   def new
     @user = User.find(params[:user_id])
     @post = @user.posts.build
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
   def show
     if params[:id] == 'sign_out'
       sign_out(current_user)
