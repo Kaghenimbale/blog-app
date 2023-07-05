bfb420afee57cf2c9bf4f2e86f59aa9906c7dccb
Kagheni <Kaghenimbale@gmail.com>
2023-07-05 11:02:34 +0200

    Add changes on the controller and models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,9 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    if params[:id] == 'sign_out'
+      sign_out(current_user)
+      redirect_to root_path, notice: 'You have been signed out successfully.'
+    else
+      @user = User.find(params[:id])
+      @first_three_posts = @user.posts.limit(3)
+      @recent_posts = @user.recent_posts
+    end
   end
 end
