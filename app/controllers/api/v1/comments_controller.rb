95e5930e2daf1aa2717281b158221cfaf430678a
Kagheni <Kaghenimbale@gmail.com>
2023-07-06 11:22:37 +0200

    Fix linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,29 +1,29 @@
 class Api::V1::CommentsController < ApplicationController
-    def index
-      @comments = Post.find(params[:post_id]).comments.all
-      render json: @comments
-    end
-  
-    def create
-      post = Post.find(params[:post_id])
-      user = User.find_by(auth_key: request.headers['Auth-Key'])
-  
-      if user.present?
-        comment = post.comments.build(author: user, text: comment_params[:text])
-  
-        if comment.save
-          render json: comment, status: :created
-        else
-          render json: comment.errors, status: :unprocessable_entity
-        end
+  def index
+    @comments = Post.find(params[:post_id]).comments.all
+    render json: @comments
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    user = User.find_by(auth_key: request.headers['Auth-Key'])
+
+    if user.present?
+      comment = post.comments.build(author: user, text: comment_params[:text])
+
+      if comment.save
+        render json: comment, status: :created
       else
-        render json: { error: 'Invalid Auth Key' }, status: :unauthorized
+        render json: comment.errors, status: :unprocessable_entity
       end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text)
+    else
+      render json: { error: 'Invalid Auth Key' }, status: :unauthorized
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    
+
     render json: @post
   end
 end
