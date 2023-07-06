348a06ea2fb3ac8c9ef74b89c6581ffe9a87db34
Kagheni <Kaghenimbale@gmail.com>
2023-07-06 09:43:00 +0200

    Add spec api test

---
+++ spec/requests/api/v1/posts_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Posts", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/api/v1/posts/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/api/v1/posts/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
